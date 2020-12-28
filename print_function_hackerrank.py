b4c1f6e4f77c20c9892c3d5e6dc2792462784e83
Sayan Mukherjee <70065597+sayan9112@users.noreply.github.com>
2020-12-28 23:30:21 +0600

    Add files via upload

---
+++ print_function_hackerrank.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    
+    def serial():
+        n= int(input())
+        
+        for i in range(1,n+1):
+           print(i,end="")
+            
+    serial()    
+        
